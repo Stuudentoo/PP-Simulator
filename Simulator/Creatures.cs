e0f8fd53eb89f7b1d7195858a0449643759e7e7b
Łukasz Gałek <lukaszgalek2004@gmail.com>
2024-10-29 18:53:07 +0100

    Stwory
    Stwory

---
+++ Simulator/Creatures.cs
@@ -0,0 +1,30 @@
+namespace Simulator
+{
+    internal class Creature
+    {
+        private string name;
+        public string Name {
+            get {return name; }
+            set {name = value;}
+        }
+        private int level;
+        public int Level {
+            get {return level; }
+            set {level = value;}
+        }
+        public Creature(){ }
+        public Creature(string name, int level = 1)
+        {
+            Name = name;
+            Level = level;
+        }
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi I'm {Name} and my level is {Level}");
+        }
+        public string Info
+        {
+            get { return $"{Name} [{Level}]"; }
+        }
+    }
+}
