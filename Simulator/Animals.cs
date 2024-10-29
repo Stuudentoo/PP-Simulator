a49e7b797a13cc3ab77c1db8a7c30c597d835ad2
Łukasz Gałek <lukaszgalek2004@gmail.com>
2024-10-29 21:04:39 +0100

    Settery
    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -1,10 +1,32 @@
 namespace Simulator
 {
     public class Animals
+{
+    private string description = "Unknown";
+
+    public required string Description
     {
-        public required string Description { get; init; }
-        public uint Size { get; set; } = 3;
-        public string Info => $"{Description} <{Size}>";
-        
+        get => description; 
+        init
+        {
+            description = value.Trim();
+            if (description.Length < 3)
+            {
+                description = description.PadRight(3, '#');
+            }
+            if (description.Length > 15)
+            {
+                description = description.Substring(0, 15);
+                description = description.Trim();
+                if (description.Length < 3)
+                {
+                    description = description.PadRight(3, '#');
+                }
+            }
+            description = char.ToUpper(description[0]) + description.Substring(1);
+        }
     }
+    public uint Size { get; set; } = 3;
+    public string Info => $"{Description} <{Size}>";
+}
 }
--- Simulator/obj/Debug/net8.0/Simulator.AssemblyInfo.cs
+++ Simulator/obj/Debug/net8.0/Simulator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f21a30109aff742e3311e5cd116f97d74aecb00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0f8fd53eb89f7b1d7195858a0449643759e7e7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
