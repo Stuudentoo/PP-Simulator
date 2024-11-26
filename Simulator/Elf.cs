4278f9813b6d18e19caf548dc81748ee1ea2e03b
Łukasz Gałek <lukaszgalek2004@gmail.com>
2024-11-26 23:29:30 +0100

    SimConsole
    SimConsole

--- Simulator/Elf.cs
+++ Simulator/Elf.cs
@@ -1,28 +1,57 @@
-﻿namespace Simulator;
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Elf : Creature
+namespace Simulator
 {
-    private int agility = 1;
-    private int singCounter = 0;
-
-    public int Agility { get => agility; init => agility = Validator.Limiter(value, 0, 10); }
-    public override int Power => 8 * Level + 2 * Agility;
-    public void Sing()
+    public class Elf : Creature
     {
-        singCounter++;
-        if (singCounter % 3 == 0)
+        private int agility = 1;
+        private int counter = 0;
+        public int Agility
         {
-            if (agility < 10)
+            get { return agility; }
+            init
             {
-                agility++;
+               agility= Validator.Limiter(value, 0, 10);
             }
-        }
-    }
 
-    public Elf() { }
-    public Elf(string name = "Unknown Elf", int level = 1, int agility = 1) : base(name, level)
-    {
-        Agility = agility;
+        }
+
+        public void Sing() {
+            counter++;
+         
+            if (counter % 3 == 0)
+            {
+                if (agility < 10) agility++;
+
+            }
+
+        }
+
+        public Elf() { }
+        public Elf(string name, int level=1, int agility=1) : base(name, level)
+        {
+            Agility = agility;
+        }
+        public override string Greeting()
+        {
+            
+            return $"Hi, I'm {Name}, my level is {Level}, my agility is {Agility}";
+         
+        }
+        public override int Power => 8 * Level + 2 * Agility;
+        public override string Info
+        {
+            get { return $"{Name} [{Level}][{Agility}]"; }
+        }
+
+
+
+
+
     }
-    public override string Info => $"{Name} [{Level}][{Agility}]";
 }
--- Simulator/Orc.cs
+++ Simulator/Orc.cs
@@ -1,28 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Simulator;
-
-public class Orc : Creature
+namespace Simulator
 {
-    private int rage = 1;
-    private int huntCounter = 0;
-
-    public int Rage { get => rage; init => rage = Validator.Limiter(value, 0, 10); }
-    public override int Power => 7 * Level + 3 * Rage;
-    public void Hunt()
+    public class Orc : Creature
     {
-        huntCounter++;
-        if (huntCounter % 2 == 0)
+
+        private int rage=1;
+        private int counter = 0;
+        public int Rage
         {
-            if (rage < 10)
-            {
-                rage++;
+            get { return rage; }
+            init {
+                rage=Validator.Limiter(value, 0, 10);
             }
         }
+
+        public void Hunt() {
+            counter++;
+            if (counter % 2 == 0)
+            {
+                if (rage < 10) rage++;
+            }
+        }
+        public Orc() { }
+        public Orc(string name, int level=1, int rage=1) : base(name, level)
+        {
+            Rage = rage;
+        }
+        public override string Greeting() {
+            
+            return $"Hi, I'm {Name}, my level is {Level}, my rage is {Rage}";
+           
+        }
+        public override string Info
+        {
+            get{ return $"{Name} [{Level}][{Rage}]"; }
+        }
+        public override int Power => 7 * Level + 3 * Rage;
+
+
+
     }
-    public Orc() { }
-    public Orc(string name = "Unknown Orc", int level = 1, int rage = 1) : base(name, level)
-    {
-        Rage = rage;
-    }
-    public override string Info => $"{Name} [{Level}][{Rage}]";
 }
