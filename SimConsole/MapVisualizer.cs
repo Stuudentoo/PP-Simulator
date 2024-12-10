126267d58df62c22ffc769042ae69cde897d3de7
Łukasz Gałek <lukaszgalek2004@gmail.com>
2024-12-10 16:21:38 +0100

    Duza Mapa
    Duza Mapa

--- SimConsole/MapVisualizer.cs
+++ SimConsole/MapVisualizer.cs
@@ -1,4 +1,5 @@
-﻿﻿﻿using Simulator.Maps;
+﻿﻿
+﻿using Simulator.Maps;
 using Simulator;
 namespace SimConsole
 {
@@ -14,8 +15,6 @@ namespace SimConsole
 
         public void Draw()
         {
-          
-
             Console.Write(Box.TopLeft);
             for (int x = 0; x < _map.SizeX; x++)
             {
@@ -24,13 +23,14 @@ namespace SimConsole
             }
             Console.WriteLine(Box.TopRight);
 
-            for (int y = _map.SizeY - 1; y >= 0; y--) 
+            // Zmiana pętli - iteracja po osi Y od 0 do SizeY-1
+            for (int y = 0; y < _map.SizeY; y++)
             {
                 Console.Write(Box.Vertical);
 
                 for (int x = 0; x < _map.SizeX; x++)
                 {
-                    var creatures =_map.At(new Point(x, y));
+                    var creatures = _map.At(new Point(x, y));
                     if (creatures.Count > 1)
                     {
                         Console.Write('X');
@@ -39,7 +39,6 @@ namespace SimConsole
                     {
                         var creature = creatures[0];
                         Console.Write($"{creature.Symbol}");
-
                     }
                     else
                     {
@@ -54,7 +53,7 @@ namespace SimConsole
 
                 Console.WriteLine(Box.Vertical);
 
-                if (y > 0)
+                if (y < _map.SizeY - 1)
                 {
                     Console.Write(Box.MidLeft);
                     for (int x = 0; x < _map.SizeX; x++)
@@ -74,7 +73,6 @@ namespace SimConsole
             }
             Console.WriteLine(Box.BottomRight);
         }
-
-
     }
-}// można zamienic na biblioteke linie 
+}
+// można zamienic na biblioteke linie 
